d9414b9225eda84f6bf17d6f38145ba8e8abffe2
Abitbol Ilan <fosfor12345@hotmail.fr>
2018-12-31 16:53:36 +0100

    first correction exo basicfasm

--- 3_basic_fasm/3_exercises/3_fix_programs/0_broken_adder.asm
+++ 3_basic_fasm/3_exercises/3_fix_programs/0_broken_adder.asm
@@ -1,23 +1,23 @@
 ; Basic fasm
 ; ==========
-; 
+;
 ; Fix Programs
 ; ------------
 ;
 ; Broken Adder
 ; @@@@@@@@@@@@
-; 
+;
 ; 0.    Try to assemble and find out the assembly error message.
-;
+;       esi fait 32 bits et al (eax:ah,al) fait 16 bits
 ; 1.    Fix the code to make the assembly process succeed.
-;
+;       mov     esi,eax
 ; 2.    Run the program. Make sure that the program behaves correctly.
 ;
 
 format PE console
 entry start
 
-include 'win32a.inc' 
+include 'win32a.inc'
 
 ; ===============================================
 section '.text' code readable executable
@@ -26,15 +26,15 @@ start:
     ; The program begins here:
 
     ; Read two numbers:
-    call    read_hex    
-    mov     esi,al
+    call    read_hex
+    mov     esi,eax
     call    read_hex
 
     ; Add the two numbers, to get their sum.
     add     eax,esi
 
     ; Output the sum:
-    call    print_eax   
+    call    print_eax
 
     ; Exit the process:
 	push	0
