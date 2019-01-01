819ae830de26462592fe1d6e2bdc6c61be8b60e9
Ilan Abitbol <fosfor12345@hotmail.fr>
2019-01-01 18:42:51 +0100

    exercise 3_basic_fasm/3_exercies/3_fix_programs/1_lame_formula.asm

--- 3_basic_fasm/3_exercises/3_fix_programs/1_lame_formula.asm
+++ 3_basic_fasm/3_exercises/3_fix_programs/1_lame_formula.asm
@@ -1,23 +1,23 @@
 ; Basic fasm
 ; ==========
-; 
+;
 ; Fix Programs
 ; ------------
 ;
 ; Lame Formula
 ; @@@@@@@@@@@@
-; 
+;
 ; 0.    Try to assemble and find out the assembly error message.
 ;
 ; 1.    Fix the code to make the assembly process succeed.
-;
+;       mult est une instruction qui n'existe pas, il faut utiliser "mul"
 ; 2.    Run the program. Make sure that the program behaves correctly.
 ;
 
 format PE console
 entry start
 
-include 'win32a.inc' 
+include 'win32a.inc'
 
 ; ===============================================
 section '.text' code readable executable
@@ -26,17 +26,17 @@ start:
     ; The program begins here:
 
     ; Read three numbers a,b,c:
-    call    read_hex    ; a
-    mov     ebx,eax
-    call    read_hex    ; b
-    mov     ecx,eax
-    call    read_hex    ; c
+    call    read_hex    ;eax=a
+    mov     ebx,eax     ;eax=a ebx=a
+    call    read_hex    ;eax=b ebx=a
+    mov     ecx,eax     ;eax=b ebx=a ecx=b
+    call    read_hex    ;eax=c ebx=a ecx=b
 
-    mult    ecx         ; b * c
-    add     eax,ebx     ; (b * c) + a
+    mul     ecx         ;eax=c*b ebx=a ecx=b [eax]*[ecx]->[edx:eax]
+    add     eax,ebx     ;eax=(c*b)+a ebx=a ecx=b (b * c) + a
 
     ; Output the result (b * c) + a
-    call    print_eax   
+    call    print_eax
 
     ; Exit the process:
 	push	0
