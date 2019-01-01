691b364cda9f6063b3ab63ca251d8a44a30054b1
Ilan Abitbol <fosfor12345@hotmail.fr>
2019-01-01 19:28:40 +0100

    exercise 3_basic_fasm/3_exercies/3_fix_programs/2_magic_numbej.asm

--- 3_basic_fasm/3_exercises/3_fix_programs/2_magic_numbej.asm
+++ 3_basic_fasm/3_exercises/3_fix_programs/2_magic_numbej.asm
@@ -1,12 +1,12 @@
 ; Basic fasm
 ; ==========
-; 
+;
 ; Fix Programs
 ; ------------
 ;
 ; Magic numbej
 ; @@@@@@@@@@@@
-; 
+;
 ; 0.    Try to assemble and find out the assembly error message.
 ;
 ; 1.    Fix the code to make the assembly process succeed.
@@ -17,7 +17,7 @@
 format PE console
 entry start
 
-include 'win32a.inc' 
+include 'win32a.inc'
 
 ; ===============================================
 section '.text' code readable executable
@@ -26,10 +26,10 @@ start:
     ; The program begins here:
 
     call    read_hex    ; Read input.
-    add     eax,100j    ; Add 100 hex to the input.
+    add     eax,100    ; Add 100 hex to the input.
 
     ; Output the result:
-    call    print_eax   
+    call    print_eax
 
     ; Exit the process:
 	push	0
